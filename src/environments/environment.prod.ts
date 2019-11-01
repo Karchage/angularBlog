6ce148db849473d93b1ff2a6e1f08927c212f125
Karchage <glebkul2010@yandex.ru>
2019-11-01 13:13:48 +0300

    Load in git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCBFTlCiYApXKfF6FHmHeZ0jujf5k5Sj94',
+  fbDbUrl: 'https://angular-blog-861e9.firebaseio.com',
+};
